d028bcbbc7f99cc2c7429fc58134f0e8476ecfa4
bokyoungp <bokyoungp@gmail.com>
2026-05-29 14:23:35 +0900

    ëentityGraph

--- src/main/java/com/example/spring_data_jpa/repository/ItemRepository.java
+++ src/main/java/com/example/spring_data_jpa/repository/ItemRepository.java
@@ -1,8 +1,14 @@
 package com.example.spring_data_jpa.repository;
 
 import com.example.spring_data_jpa.domain.Item;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
   Item findByItemId(long itemId);
+
+  @EntityGraph(attributePaths = {"category"}) // category 도 left outer join 으로 함께 로딩
+  List<Item> findAll();
 }
